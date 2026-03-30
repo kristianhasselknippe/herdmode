8d5c0f5f79841000eb504aab99c6539c999c6106
Kristian Fjeld Hasselknippe <kristian@midio.com>
2026-03-30 17:27:50 +0200

    chore(web): configure Vite build output for static serving

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../web/dist",
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3001",
